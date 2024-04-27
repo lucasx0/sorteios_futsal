21131e9c502d8ee1180c9b6efde4f49f17b7975f
lucasx <lucas.almeidasilva49@gmail.com>
2024-04-27 20:00:21 -0300

    alterando o cadastro

--- src/main/java/com/sort_futsal/sorteios_futsal/model/Cadastro.java
+++ src/main/java/com/sort_futsal/sorteios_futsal/model/Cadastro.java
@@ -20,7 +20,7 @@ public class Cadastro {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private String nome;
     private String email;
     private String senha;

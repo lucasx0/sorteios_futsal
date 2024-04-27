353da4aec3ff8530ddfe78ad13c452edb4e9e5f7
lucasx <lucas.almeidasilva49@gmail.com>
2024-04-27 19:13:11 -0300

    criando repository

---
+++ src/main/java/com/sort_futsal/sorteios_futsal/repository/cadastroRepository.java
@@ -0,0 +1,11 @@
+package com.sort_futsal.sorteios_futsal.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sort_futsal.sorteios_futsal.model.Cadastro;
+
+@Repository
+public interface cadastroRepository extends CrudRepository<Cadastro, Long>{
+    
+}

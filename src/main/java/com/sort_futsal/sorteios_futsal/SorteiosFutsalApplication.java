94ad792a06dd2f5ad5194740587302bc21ca7540
lucasx <lucas.almeidasilva49@gmail.com>
2024-04-27 19:01:55 -0300

    iniciando projeto

---
+++ src/main/java/com/sort_futsal/sorteios_futsal/SorteiosFutsalApplication.java
@@ -0,0 +1,13 @@
+package com.sort_futsal.sorteios_futsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorteiosFutsalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorteiosFutsalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sort_futsal/sorteios_futsal/SorteiosFutsalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sort_futsal.sorteios_futsal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorteiosFutsalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

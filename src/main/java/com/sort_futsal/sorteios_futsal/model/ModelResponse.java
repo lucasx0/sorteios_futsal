3ae8a2a99d3d044ff621f1ad7f393eb410988345
lucasx <lucas.almeidasilva49@gmail.com>
2024-04-27 20:00:08 -0300

    model response (mensagens de erro)

---
+++ src/main/java/com/sort_futsal/sorteios_futsal/model/ModelResponse.java
@@ -0,0 +1,14 @@
+package com.sort_futsal.sorteios_futsal.model;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+public class ModelResponse {
+    
+    private String Mensagem;
+}
